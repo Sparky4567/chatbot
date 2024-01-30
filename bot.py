7696abb76ad6352adf67eee587d26db8c9e9db30
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2024-01-30 14:56:59 +0200

    configuration changes, speak back function, multipple answers, voice recognition

--- bot.py
+++ bot.py
@@ -2,6 +2,12 @@ import sqlite3
 from fuzzywuzzy import fuzz, process
 import subprocess
 import random
+from googletrans import Translator
+import requests
+import speech_recognition as sr
+from config import USE_TRANSLATION_SERVICE
+from config import SPEAK_BACK
+from config import USE_VOICE_INPUT
 # Connect to SQLite database (or create it if it doesn't exist)
 conn = sqlite3.connect('chatbot_database.db')
 cursor = conn.cursor()
@@ -26,6 +32,22 @@ cursor.execute('''
 
 conn.commit()
 
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    recognizer.dynamic_energy_threshold = False
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("\n\nSay something:\n\n")
+        audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        return None
+    except sr.RequestError as e:
+        return None
+
+
 def get_answers_from_database(question):
     # Check if the question exists in the database
     cursor.execute('SELECT id FROM questions WHERE question = ?', (question,))
@@ -38,6 +60,33 @@ def get_answers_from_database(question):
         return [result[0] for result in results]
     else:
         return None
+
+def is_online():
+    try:
+        # Try to send a request to a well-known server (e.g., Google's public DNS server)
+        response = requests.get("http://8.8.8.8", timeout=5)
+        response.raise_for_status()
+        return True
+    except requests.RequestException:
+        return False
+
+def return_translated_text(passed_phrase):
+    if(USE_TRANSLATION_SERVICE is True):
+        translator = Translator()
+        # Detect the language of the input text
+        detected_language = translator.detect(passed_phrase).lang
+        # Translate the text to English (you can choose a different target language if needed)
+        translated_text = translator.translate(passed_phrase, src=detected_language, dest='en').text
+        user_approval=str(input(str("\n\nDo you want to approve translation: {}? (y)\n\n").format(translated_text)))
+        if(user_approval == "y"):
+            return translated_text
+        else:
+            user_input=str(input("\n\nWrite your answer:\n\n"))
+            return user_input
+    else:
+        return passed_phrase
+
+
 
 def find_best_match(question):
     # Check if the question exists in the database
@@ -68,26 +117,61 @@ def save_question_and_answers_to_database(question, answers):
     question_id = cursor.fetchone()[0]
 
     # Save each answer to the answers table, linked to the question
-    if(len(answers)>1):
-        for answer in answers:
-            cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answer).lower()))
+    if(is_online is False):
+        if(len(answers)>1):
+            for answer in answers:  
+                cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answer).lower()))
+                conn.commit()
+        else:
+            cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answers[0]).lower()))
             conn.commit()
     else:
-        cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answers[0]).lower()))
-        conn.commit()
+        if(len(answers)>1):
+            for answer in answers:  
+                answer = return_translated_text(answer)
+                cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answer).lower()))
+                conn.commit()
+        else:
+            answer = return_translated_text(answers[0])
+            cursor.execute('INSERT INTO answers (question_id, answer) VALUES (?, ?)', (question_id, str(answer).lower()))
+            conn.commit()
+
 
 def speak_back(answer):
-    model = "en_US-amy-medium.onnx"        
-    command = "cd ./venv/piper && echo '{}' | \
-    ./piper --model {} --output-raw | \
-    aplay -r 22050 -f S16_LE -t raw -".format(answer,model)
-    result = subprocess.run(command, shell=True, check=True, text=True)
+    if(SPEAK_BACK is True):
+        model = "en_US-amy-medium.onnx"        
+        command = "cd ./venv/piper && echo '{}' | \
+        ./piper --model {} --output-raw | \
+        aplay -r 22050 -f S16_LE -t raw -".format(answer,model)
+        result = subprocess.run(command, shell=True, check=True, text=True)
 
+   
+def recognizer():
+    user_input = input("\n\nPress 'r' to record\n\n")
+
+    if user_input.lower() == 'r':
+        text = recognize_speech()
+
+        if text:
+            # Use the 'text' variable for further processing
+            print(str("\n\nRecognized text:{}\n\n").format(text))
+            user_input=input("\n\nWant to approve question ? (y)\n\n")
+            if(str(user_input).lower()=="y"):
+                recognized_phrase = text
+                return recognized_phrase
+        else:
+            print("\n\nI did not manage to understand your voice input. Reinitiating.\n\n")
+            recognizer()
+    else:
+        recognizer()
 def chatbot():
     print("Welcome to the Chatbot! Type 'exit' to end the conversation.")
 
     while True:
-        user_input = input("You: ")
+        if(USE_VOICE_INPUT is True):
+            user_input = recognizer()
+        else:
+            user_input = input("You: ")
 
         # Check for exit command
         if user_input.lower() == 'exit':
---
+++ config.py
@@ -0,0 +1,8 @@
+# ========== MAIN SETTINGS ==========
+
+USE_TRANSLATION_SERVICE = False
+SPEAK_BACK = True
+
+# ========== VOICE INPUT ==========
+
+USE_VOICE_INPUT = True
