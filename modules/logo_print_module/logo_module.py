f4283d8341fde1e21e12a333ecb275a0002d5e5e
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2024-02-04 15:06:23 +0200

    changed printed logo

--- modules/logo_print_module/logo_module.py
+++ modules/logo_print_module/logo_module.py
@@ -2,7 +2,7 @@ from pyfiglet import Figlet
 
 class Logo_Module:
     def __init__(self):
-        self.name = "Amy Bot"
+        self.name = "White Rabbit"
 
     def print_logo(self):
         f = Figlet(font="slant")
