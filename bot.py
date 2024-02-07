6b8938a2e898c72b6c25345bbff137664bb059de
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2024-02-07 13:38:46 +0200

    graceful exit

--- bot.py
+++ bot.py
@@ -4,4 +4,11 @@ from modules.main_bot_module.main_module import Main_Module
 
 bot = Main_Module()
 bot.manage_database_creation()
-bot.chatbot()
+try:
+    while True:
+        bot.chatbot()
+        pass
+except KeyboardInterrupt:
+    print("\n\nCtrl+C detected. Exiting gracefully...\n\n")
+    quit()
+
